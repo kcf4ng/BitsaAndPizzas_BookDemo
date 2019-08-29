707cc9ab1fa01bb68ecc27fcd3f7dbd42bbe37fb
kcfang <kcf4ng@gmail.com>
2019-08-29 09:49:03 +0800

    0829 commit

--- app/src/main/java/com/hfad/bitsandpizzas/MainActivity.java
+++ app/src/main/java/com/hfad/bitsandpizzas/MainActivity.java
@@ -1,16 +1,24 @@
 package com.hfad.bitsandpizzas;
 
 import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.MenuItemCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
     ShareActionProvider shareActionProvider;
 
     @Override
@@ -47,6 +55,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         InitialComponent();
+        SectionsPagerAdapter pagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        ViewPager pager = findViewById(R.id.pager);
+        pager.setAdapter(pagerAdapter);
+
+        TabLayout tabLayout = findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(pager);
+
 
     }
 
@@ -56,4 +71,58 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private class SectionsPagerAdapter extends FragmentPagerAdapter{
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            switch(i){
+                case 0:
+                    Log.d(TAG,"frag home");
+                    return new TopFragment();
+                case 1:
+                    Log.d(TAG,"frag piza");
+                    return new PizzaFragment();
+                case 2:
+                    Log.d(TAG,"frag Pasta");
+                    return new PastaFragment();
+                case 3:
+                    Log.d(TAG,"frag Stores");
+                    return new StoresFragment();
+
+            }
+
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 4;
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch(position){
+                case 0:
+
+                    return getResources().getText(R.string.home_tab);
+                    case 1:
+
+                    return getResources().getText(R.string.pizza_tab);
+                case 2:
+
+                    return getResources().getText(R.string.pasta_tab);
+                case 3:
+
+                    return getResources().getText(R.string.store_tab);
+
+            }
+
+            return super.getPageTitle(position);
+        }
+    }
 }
